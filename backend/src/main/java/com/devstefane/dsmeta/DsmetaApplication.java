fc3fbb81bee77fc3de5cd7b01a19680260347f07
Stefane M <stefane.maria@alu.ufc.br>
2022-09-08 15:18:07 -0300

    Solving SpringTools problem

--- backend/src/main/java/com/devstefane/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devstefane/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package com.devsupeior.dsmeta;
+package com.devstefane.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
